0285f2cd02a7ee9820f366c0dcf38b4a1590d2ab
akki <akki>
2020-02-10 18:23:56 +0530

    initial commit

---
+++ mongscript.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+
+
+client = MongoClient('mongodb://localhost:27017')
+
+
+db = client['jacaransa-db']
+
+
+def aggregationmethod(collection):
+    aggregation_conditions = [ 
+    {"$group": {
+            "_id": {"district": "$district","price['value']":"$price.value","price['currency']":"$price.currency"},
+            "uniqueIds": {"$push": "$_id"},
+            "count":{"$sum":1}
+            }
+        },
+        {"$match": { 
+            "count": {"$gt": 1}
+            }
+        },{
+        "$project":{
+            "_id":0,
+            "uniqueIds":1
+        }
+        }]
+
+    results = collection.aggregate(aggregation_conditions)
+
+    for r in results:
+        print(r)
+
+
+
+formatted_properties = db.formatted_properties
+
+
+aggregationmethod(formatted_properties)
+
+
