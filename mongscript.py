03f1d476152625451e845faee6d842e8d06358bb
A.sharma <akshay@mastrolinks.com>
2020-02-11 11:44:38 +0530

    change in query

--- mongscript.py
+++ mongscript.py
@@ -1,18 +1,22 @@
 from pymongo import MongoClient
+from bson.raw_bson import RawBSONDocument
+from bson.codec_options import CodecOptions
 
 client = MongoClient()
 
-
+codec_options = CodecOptions(document_class=RawBSONDocument)
 client = MongoClient('mongodb://localhost:27017')
 
 
-db = client['jacaransa-db']
+db = client['jacaranda-db']
 
 
 def aggregationmethod(collection):
     aggregation_conditions = [ 
     {"$group": {
             "_id": {"district": "$district","price['value']":"$price.value","price['currency']":"$price.currency"},
+            "name":{"$addToSet":"$title"},
+            "_id":{"addToSet": "$_id"},
             "uniqueIds": {"$push": "$_id"},
             "count":{"$sum":1}
             }
@@ -20,25 +24,17 @@ def aggregationmethod(collection):
         {"$match": { 
             "count": {"$gt": 1}
             }
-        },
-        {
-            "$merg"
         },{
         "$project":{
             "_id":0,
-            "uniqueIds":1
+            "uniqueIds":1,
+            "name":1
         }
         }]
 
     results = collection.aggregate(aggregation_conditions)
-
-    for r in results:
-    #    data = collection.aggregate([
-    #         {"$match":{
-    #             "_id":{"$in":r.uniqueIds}
-    #         }}
-    #     ])
-       print(r)
+    mycol = mydb["customers"]
+    mycol.insert_many(results)
 
 
 
